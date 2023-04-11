8ff2adb26d61f7657a9aaf2e7bbe9a79286a9489
TaManhhh <manhta2272001@gmail.com>
2023-04-11 23:49:23 +0700

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { UserIcon, ChevronDownIcon,SearchIcon,AdjustmentsIcon,} from "react-native-heroicons/outline";
+const HomeScreen = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  return (
+    <SafeAreaView>
+      <Text className="text-red-700">
+        <View>
+          <Image
+            source={{ uri: "https://links.papareact.com/wru" }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+        </View>
+        <View>
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className=" font-bold text-xl"> Current Location
+            <ChevronDownIcon size={20} color='#00CCBB'/>
+          </Text>
+        </View>
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
